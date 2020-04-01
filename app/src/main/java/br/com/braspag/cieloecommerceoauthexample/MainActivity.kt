0f331c5657a214f7939c9ef353f0152ce6c36a3f
Maurici <m.f.j@hotmail.com>
2020-04-01 15:54:48 -0300

    Library name changed

--- app/src/main/java/br/com/braspag/cieloecommerceoauthexample/MainActivity.kt
+++ app/src/main/java/br/com/braspag/cieloecommerceoauthexample/MainActivity.kt
@@ -2,14 +2,15 @@ package br.com.braspag.cieloecommerceoauthexample
 
 import android.app.Activity
 import android.os.Bundle
-import br.com.braspag.cieloecommerceoauthexample.network.Environment
-import br.com.braspag.cieloecommerceoauthexample.network.HttpCredentialsClient
+import br.com.braspag.cieloecommerceoauth.network.Environment
+import br.com.braspag.cieloecommerceoauth.network.HttpCredentialsClient
 import kotlinx.android.synthetic.main.activity_main.textView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val client = HttpCredentialsClient(
             Environment.SANDBOX,
             "df66638b-3ef4-421f-a18e-e20dea38d97d",
